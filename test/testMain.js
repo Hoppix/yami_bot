5bbed1249714fc6cfbc465e688b765c448d67e31
Hoppix <k.hopfmann@hotmail.de>
2019-11-29 01:16:08 +0100

    basic test framework

---
+++ test/testMain.js
@@ -0,0 +1,9 @@
+/**
+ * Created by khopf on 29/11/2019.
+ */
+var assert = require('assert');
+describe('Default', function() {
+		it('Just running empty tests', function(){
+			assert.equal(true, true);
+		});
+});
