52db02955bdbc950e9b24fa4d2fe72d0afcf4fcc
hoppix <k.hopfmann@hotmail.de>
2022-10-24 21:12:14 +0200

    uugh

--- ci/smoke_test.sh
+++ ci/smoke_test.sh
@@ -5,18 +5,19 @@ set -u
 set -o pipefail
 set -x
 
-sleep 60 # wait a minute for the container to run
+sleep 10 # wait a minute for the container to run
 
 DOCKER_RUNS=$(docker ps --all | grep yami_bot) 
+CONTAINER_ID=${DOCKER_RUNS:0:3}
 
 if [[ ! $DOCKER_RUNS == *"Exited"* ]]; then
 	echo "The container is not running"
   exit 1
 fi
 
-DOCKER_BOT_STARTED=$(docker logs yami_bot | grep "Startup time:")
-echo $(docker logs yami_bot)
- 
+DOCKER_BOT_STARTED=$(docker logs $CONTAINER_ID | grep "Startup time:")
+echo $(docker logs $CONTAINER_ID)
+
 if [[ -z $DOCKER_BOT_STARTED ]]; then
 	echo "There where no startup logs"
 	exit 1
